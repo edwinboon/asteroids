b4cdbfda529010fdef7463afb367c8c49b1f8eb6
Edwin Boon <hello@edwinboon.dev>
2025-07-16 15:42:13 +0200

    limit to 60fps

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     # Main game loop
     while True:
         for event in pygame.event.get():
@@ -15,6 +18,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000  # Convert milliseconds to seconds
 
 
 if __name__ == "__main__":
