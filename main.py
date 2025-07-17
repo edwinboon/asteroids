61a83b1296ca8ee4da3bb6233972dc567168ec63
Edwin Boon <hello@edwinboon.dev>
2025-07-17 08:11:59 +0200

    Render player on the screen

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 
     clock = pygame.time.Clock()
     dt = 0
@@ -17,6 +19,7 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000  # Convert milliseconds to seconds
